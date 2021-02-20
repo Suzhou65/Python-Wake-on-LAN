387ef12e44ad74269e6e0ac594fce2b8c5ea45d9
Suzhou65 <takahashikagawa@gmail.com>
2021-02-21 00:02:10 +0800

    upload pypl needed version

---
+++ wakeonlan_status.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <!-- META -->
+        <meta http-equiv="Cache-Control" content="No-Cache"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <!-- Style -->
+        <style>
+            html{font-size:100%; font-family:sans-serif; color:#666666;}
+            body{margin-top:10%;}
+            a{font-size:0.917rem; color:#8DB6CD; text-decoration:none;}
+            .fail{margin:0px auto; text-align:center; font-size:1rem; color:#FF5733; font-weight:bold; padding:1rem;}
+            .footer{margin:0px auto; text-align:center; font-size:0.8rem; padding:1rem;}
+            /*Table*/
+            table.csv_output{margin:0px auto; border:1px solid #FFFFFF; width:80%; border-collapse:collapse;}
+                tr{background:#FFFFFF; height:25px; border-left:none; padding:3px 2px;}
+                /*Header*/
+                tr:nth-child(1){background:#0B6FA4; color:#FFFFFF; border-left:none; padding:3px 2px;}
+                /*Column*/
+                td{padding:0.3rem;}
+                td:nth-child(1){text-align:center; min-width:12rem;}
+                td:nth-child(2){text-align:center; font-weight:bold; min-width:12rem;}
+                td:nth-child(3){text-align:center; min-width:12rem;}
+        </style>
+        <!-- Title -->
+        <title>Forwarding Status</title>
+    </head>
+    <!-- Body -->
+    <body>
+        <!-- Load CSV file -->
+        <table class="csv_output">
+            <!-- php block -->
+            <?php
+            // File path as you python script location
+            if ($file = fopen("/python_script_location/wakeup_record.csv","r")){
+                while (($line = fgetcsv($file)) !== false){
+                    echo "<tr>";
+                    foreach ($line as $cell) {echo "<td>" . htmlspecialchars($cell) . "</td>";}
+                    echo "</tr>\n";}
+                    fclose($file);
+                }else{
+                    // If unable open file
+                    echo '<div class="fail">';
+                    echo "Unable to open monitoring file.";
+                    echo "</div>";}
+            ?>
+            <!-- php block -->
+        </table>
+        <div class="footer"><a href="https://github.com/Suzhou65/Python-Wake-on-LAN">Python-Wake-on-LAN</a></div>
+    </body>
+</html>
